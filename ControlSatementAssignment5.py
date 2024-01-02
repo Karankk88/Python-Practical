2ac8fc414cc0d565fc2047509224ac4ff717babb
Karankk88 <155479217+Karankk88@users.noreply.github.com>
2024-01-03 00:22:54 +0530

    Add files via upload

---
+++ ControlSatementAssignment5.py
@@ -0,0 +1,107 @@
+#1
+'''
+for i in range(3):
+    for j in range(3):
+        print("*",end=' ')
+    print()
+'''
+
+#2
+'''
+num=1
+for i in range(4):
+    for j in range(4):
+        print(num,end=' ')
+        num=num+1
+    print()
+'''
+
+#3
+'''
+num=1
+for i in range(4):
+    
+    for j in range(4):
+        print(num, end=' ')
+    num=num+1  
+    print()
+'''
+
+#4
+'''
+for i in range(4):
+    num=1
+    for j in range(4):
+        print(num, end=' ')
+        num=num+1
+    print()
+'''
+
+
+#5
+'''
+num=1
+for i in range(4):
+    num=i+1
+    for j in range(4):
+      print(num,end='')
+      num=num+1
+    print()
+'''
+
+#6
+'''
+for i in range(5):
+    for j in range(5):
+        if i%2==0:
+            print('#',end='')
+        else:
+            print('@',end='')
+    print()
+'''
+
+
+#7
+'''
+num=1
+for i in range(3):
+    for j in range(3):
+       print(num, end=' ')
+       num=num+2
+    print()
+'''
+
+#8
+'''
+for i in range(3):
+    num=1
+    for j in range(3):
+        print(num,end=' ')
+        num=num+2
+    print()
+'''
+
+#9
+'''
+num=1
+for i in range(3):
+  
+    for j in range(3):
+        print(num,end=' ')
+        num=num+2
+    num=num-4
+    print()
+'''
+
+# 10
+'''
+num=1
+for i in range(4):
+  
+    for j in range(4):
+        print(num,end=' ')
+        num=num+2
+    num=num-4
+    print()
+'''
+print("Thank you!!!!!!!!!")
